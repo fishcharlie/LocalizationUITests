7fb9d3dd74c3ae94a164f7b6cf559d2ffead08fe
Charlie Fish <fishcharlie@me.com>
2017-07-10 00:25:14 -0600

    Adding support for Snapshot and reverting to iOS 10, Xcode 8.3.3 and Swift 3

--- LocalizationUITests/LocalizationUITests.swift
+++ LocalizationUITests/LocalizationUITests.swift
@@ -26,9 +26,12 @@ class LocalizationUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+		
+		let app = XCUIApplication()
+		setupSnapshot(app)
+		app.launch()
     }
     
     override func tearDown() {
@@ -39,6 +42,8 @@ class LocalizationUITests: XCTestCase {
     func testExample() {
         let greeting = localizedString(key: "Greetings.Hello")
         XCTAssert(XCUIApplication().staticTexts[greeting].exists, "Expected \"\(greeting)\" label to exist")
+		
+		snapshot("01Main")
     }
     
 }
